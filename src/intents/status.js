26a3f9c5c5b76123994ac697d9c4339fd3921c5d
martincostello <martin@martincostello.com>
2017-01-29 16:38:07 +0000

    Start implementing the disruption intent
    Start implementing the intent for disruption.
    Currently supports a subset of the lines and only specifically reports a
    good status and closed lines; other statuses are treated as
    "disruption".

--- src/intents/status.js
+++ src/intents/status.js
@@ -3,7 +3,56 @@
 
 "use strict";
 
+var api = require("./../api");
 var responses = require("./../responses");
+
+var mapLineToId = function (line) {
+
+  var normalized = line.toLowerCase();
+
+  switch (normalized) {
+
+    case "bakerloo":
+    case "central":
+    case "circle":
+    case "district":
+    case "jubilee":
+    case "metropolitan":
+    case "northern":
+    case "piccadilly":
+    case "victoria":
+      return normalized;
+
+    case "hammersmith and city":
+      return "hammersmith-city";
+
+    case "waterloo and city":
+      return "waterloo-city";
+
+    default:
+      return null;
+  }
+}
+
+var generateResponse = function (data) {
+  if (data.length > 0) {
+    var line = data[0];
+    if (line.lineStatuses.length > 0) {
+      var status = line.lineStatuses[0];
+      switch (status.statusSeverity) {
+        case 10:
+          return "There is currently a good service on the " + line.name + " line.";
+
+        case 20:
+          return "The " + line.name + " line is currently closed.";
+
+        default:
+          return "There is currently disruption on the " + line.name + " line.";
+      }
+    }
+  }
+  return responses.onError;
+};
 
 var intent = {
   name: "StatusIntent",
@@ -12,14 +61,18 @@ var intent = {
     "LINE": "LINE_NAME"
   },
   utterances: [
-    "what is the status of the {LINE_NAMES|LINE}"
+    "{|what is the} status of the {-|LINE} {|line}"
   ],
   handler: function (request, response) {
-    var line = request.slot("LINE");
+    var line = mapLineToId(request.slot("LINE"));
     if (line) {
-      response
-        .say("I'm sorry, I can't tell you the status of " + line + " at the moment.")
-        .reprompt(responses.onUnknown);
+      api.getLineStatus(line)
+        .then(function (data) {
+          response
+            .say(generateResponse(data))
+            .send();
+        });
+      return false;
     } else {
       response.say(responses.onUnknown);
     }
