cddfe57e48c4bfb5f7fe3ab151f65c51dbe12dfc
martincostello <martin@martincostello.com>
2017-01-29 11:51:33 +0000

    Add server for testing
    Add test server for Alexa App Server.

---
+++ server.js
@@ -0,0 +1,16 @@
+// Copyright (c) Martin Costello, 2017. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+var AlexaAppServer = require("alexa-app-server");
+
+AlexaAppServer.start({
+  server_root: __dirname,
+  public_html: "static",
+  app_dir: "apps",
+  app_root: "/",
+  port: 3001,
+  preRequest: function (json, request, response) {
+  },
+  postRequest: function (json, request, response) {
+  }
+});
