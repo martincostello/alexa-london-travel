507f995b9c8ee5f63002d112dfd7841ee44ed148
martincostello <martin@martincostello.com>
2019-10-27 20:08:28 +0000

    Manually zip the package
    Manually zip the package to try and fix permissions issue.

--- build.sh
+++ build.sh
@@ -44,4 +44,5 @@ dotnet build ./LondonTravel.Skill.sln --output $artifacts --configuration $confi
 dotnet test ./test/LondonTravel.Skill.Tests/LondonTravel.Skill.Tests.csproj --output $artifacts --configuration $configuration || exit 1
 
 dotnet publish ./src/LondonTravel.Skill/LondonTravel.Skill.csproj --output $artifacts/publish --configuration $configuration --runtime linux-x64 --self-contained true /p:AssemblyName=bootstrap /p:PublishReadyToRun=true || exit 1
-chmod +x $artifacts/publish/bootstrap
+
+zip $artifacts/alexa-london-travel.zip $artifacts/publish
