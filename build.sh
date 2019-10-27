0948e8b8ba89a2b8a868c971c7b6a4c7120ac731
martincostello <martin@martincostello.com>
2019-10-27 20:02:57 +0000

    Fix permissions
    Try and fix permissions for the deployment.

--- build.sh
+++ build.sh
@@ -44,3 +44,4 @@ dotnet build ./LondonTravel.Skill.sln --output $artifacts --configuration $confi
 dotnet test ./test/LondonTravel.Skill.Tests/LondonTravel.Skill.Tests.csproj --output $artifacts --configuration $configuration || exit 1
 
 dotnet publish ./src/LondonTravel.Skill/LondonTravel.Skill.csproj --output $artifacts/publish --configuration $configuration --runtime linux-x64 --self-contained true /p:AssemblyName=bootstrap /p:PublishReadyToRun=true || exit 1
+chmod +x $artifacts/publish/bootstrap
