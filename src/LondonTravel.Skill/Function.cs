9b02a246523fb85309daa4e5affb1ead64a15e90
martincostello <martin@martincostello.com>
2019-11-02 16:14:22 +0000

    Simplify code
    Simplify the code that sets up the handler wrapper.

--- src/LondonTravel.Skill/Function.cs
+++ src/LondonTravel.Skill/Function.cs
@@ -1,11 +1,9 @@
 // Copyright (c) Martin Costello, 2017. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using System;
 using System.Threading.Tasks;
 using Alexa.NET.Request;
 using Alexa.NET.Response;
-using Amazon.Lambda.Core;
 using Amazon.Lambda.RuntimeSupport;
 using Amazon.Lambda.Serialization.Json;
 
@@ -31,9 +29,7 @@ namespace MartinCostello.LondonTravel.Skill
             var serializer = new JsonSerializer();
             var function = new AlexaFunction();
 
-            Func<SkillRequest, ILambdaContext, Task<SkillResponse>> handler = function.HandlerAsync;
-
-            using var handlerWrapper = HandlerWrapper.GetHandlerWrapper(handler, serializer);
+            using var handlerWrapper = HandlerWrapper.GetHandlerWrapper<SkillRequest, SkillResponse>(function.HandlerAsync, serializer);
             using var bootstrap = new LambdaBootstrap(handlerWrapper);
 
             await bootstrap.RunAsync();
