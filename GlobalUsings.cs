4dd6bb7f3b3533ef6a8e3d38a626fe72c3df2d03
martincostello <martin@martincostello.com>
2021-08-21 16:56:43 +0100

    Update to .NET 6 RC1 daily build
    Update to a daily build of release candidate 1 of .NET 6.

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -5,6 +5,7 @@ global using System;
 global using System.Collections.Generic;
 global using System.Globalization;
 global using System.Linq;
+global using System.Net.Http;
 global using System.Threading;
 global using System.Threading.Tasks;
 
