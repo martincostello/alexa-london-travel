8882fafbcaf48aba2410e443764b57eeafa28176
martincostello <martin@martincostello.com>
2021-11-26 17:47:18 +0000

    Use JSON source generator
    Use the new JSON source generator in .NET 6.

---
+++ src/LondonTravel.Skill/Extensions/ApplicationJsonSerializerContext.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Martin Costello, 2017. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+using MartinCostello.LondonTravel.Skill.Clients;
+
+namespace MartinCostello.LondonTravel.Skill.Extensions;
+
+[ExcludeFromCodeCoverage]
+[JsonSerializable(typeof(IList<Line>))]
+[JsonSerializable(typeof(IList<ServiceDisruption>))]
+[JsonSerializable(typeof(SkillUserPreferences))]
+internal sealed partial class ApplicationJsonSerializerContext : JsonSerializerContext
+{
+}
