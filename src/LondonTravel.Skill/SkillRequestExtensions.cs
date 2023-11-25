70d46b2ded5de08c983f158106b28f1b43914ff2
martincostello <martin@martincostello.com>
2023-11-25 14:58:28 +0000

    Harden SkillRequestExtensions
    Harden `SkillRequestExtensions` against null properties on the request and make it easier to debug.

--- src/LondonTravel.Skill/SkillRequestExtensions.cs
+++ src/LondonTravel.Skill/SkillRequestExtensions.cs
@@ -2,95 +2,22 @@
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 using Alexa.NET.Request.Type;
+using SkillRequest = Alexa.NET.Request.SkillRequest;
 
 namespace MartinCostello.LondonTravel.Skill;
 
 public static class SkillRequestExtensions
 {
-    public static Alexa.NET.Request.SkillRequest FromModel(this Models.SkillRequest request)
+    public static SkillRequest FromModel(this Models.SkillRequest request)
     {
-        var result = new Alexa.NET.Request.SkillRequest()
-        {
-            Context = new()
-            {
-                System = new()
-                {
-                    ApiAccessToken = request.Context.System.ApiAccessToken,
-                    ApiEndpoint = request.Context.System.ApiEndpoint,
-                    Application = new()
-                    {
-                        ApplicationId = request.Context.System.Application.ApplicationId,
-                    },
-                    Device = new()
-                    {
-                        DeviceID = request.Context.System.Device.DeviceId,
-                        PersistentEndpointID = request.Context.System.Device.PersistentEndpointId,
-                        SupportedInterfaces = request.Context.System.Device.SupportedInterfaces,
-                    },
-                    User = new()
-                    {
-                        AccessToken = request.Context.System.User.AccessToken,
-                        Permissions = new(),
-                        UserId = request.Context.System.User.UserId,
-                    },
-                },
-            },
-            Session = new()
-            {
-                Application = new()
-                {
-                    ApplicationId = request.Session.Application.ApplicationId,
-                },
-                Attributes = request.Session.Attributes,
-                New = request.Session.New,
-                SessionId = request.Session.SessionId,
-                User = new()
-                {
-                    AccessToken = request.Session.User.AccessToken,
-                    Permissions = new(),
-                    UserId = request.Session.User.UserId,
-                },
-            },
-            Version = request.Version,
-        };
+        var result = MapRequest(request);
 
         result.Request = request.Request switch
         {
-            Models.IntentRequest intent => new IntentRequest()
-            {
-                DialogState = intent.DialogState,
-                Intent = new()
-                {
-                    Name = intent.Intent.Name,
-                    Slots = intent.Intent.Slots?.ToDictionary((p) => p.Key, (p) => new Alexa.NET.Request.Slot()
-                    {
-                        Name = p.Value.Name,
-                        Value = p.Value.Value,
-                    }),
-                },
-            },
-            Models.LaunchRequest launch => new LaunchRequest(),
-            Models.SessionEndedRequest ended => new SessionEndedRequest()
-            {
-                Reason = Enum.Parse<Reason>(ended.Reason.ToString()),
-                Error = ended.Error is null ? null : new()
-                {
-                    Message = ended.Error.Message,
-                    Type = Enum.Parse<ErrorType>(ended.Error.Type.ToString()),
-                },
-            },
-            Models.SystemExceptionRequest exception => new SystemExceptionRequest()
-            {
-                ErrorCause = new()
-                {
-                    requestId = exception.ErrorCause.RequestId,
-                },
-                Error = new()
-                {
-                    Message = exception.Error.Message,
-                    Type = Enum.Parse<ErrorType>(exception.Error.Type.ToString()),
-                },
-            },
+            Models.IntentRequest intent => MapIntent(intent),
+            Models.LaunchRequest => new LaunchRequest(),
+            Models.SessionEndedRequest session => MapSessionEnd(session),
+            Models.SystemExceptionRequest exception => MapException(exception),
             _ => new UnknownRequest(),
         };
 
@@ -98,6 +25,146 @@ public static class SkillRequestExtensions
         result.Request.RequestId = request.Request.RequestId;
         result.Request.Timestamp = request.Request.Timestamp;
         result.Request.Type = request.Request.Type;
+
+        return result;
+    }
+
+    private static SkillRequest MapRequest(Models.SkillRequest request)
+    {
+        var result = new SkillRequest() { Version = request.Version };
+
+        if (request.Context is { } context)
+        {
+            result.Context = new();
+
+            if (context.System is { } system)
+            {
+                result.Context.System = new()
+                {
+                    ApiAccessToken = system.ApiAccessToken,
+                    ApiEndpoint = system.ApiEndpoint,
+                };
+
+                if (system.Application is { } application)
+                {
+                    result.Context.System.Application = new()
+                    {
+                        ApplicationId = application.ApplicationId,
+                    };
+                }
+
+                if (system.Device is { } device)
+                {
+                    result.Context.System.Device = new()
+                    {
+                        DeviceID = device.DeviceId,
+                        PersistentEndpointID = device.PersistentEndpointId,
+                        SupportedInterfaces = device.SupportedInterfaces,
+                    };
+                }
+
+                if (system.User is { } user)
+                {
+                    result.Context.System.User = new()
+                    {
+                        AccessToken = user.AccessToken,
+                        Permissions = new(),
+                        UserId = user.UserId,
+                    };
+                }
+            }
+        }
+
+        if (request.Session is { } session)
+        {
+            result.Session = new()
+            {
+                Attributes = session.Attributes,
+                New = session.New,
+                SessionId = session.SessionId,
+            };
+
+            if (session.Application is { } application)
+            {
+                result.Session.Application = new()
+                {
+                    ApplicationId = application.ApplicationId,
+                };
+            }
+
+            if (session.User is { } user)
+            {
+                result.Session.User = new()
+                {
+                    AccessToken = user.AccessToken,
+                    Permissions = new(),
+                    UserId = user.UserId,
+                };
+            }
+        }
+
+        return result;
+    }
+
+    private static SystemExceptionRequest MapException(Models.SystemExceptionRequest exception)
+    {
+        var result = new SystemExceptionRequest();
+
+        if (exception.ErrorCause is { } cause)
+        {
+            result.ErrorCause = new() { requestId = cause.RequestId };
+        }
+
+        if (exception.Error is { } error)
+        {
+            result.Error = new()
+            {
+                Message = error.Message,
+                Type = Enum.Parse<ErrorType>(error.Type.ToString()),
+            };
+        }
+
+        return result;
+    }
+
+    private static IntentRequest MapIntent(Models.IntentRequest request)
+    {
+        var result = new IntentRequest()
+        {
+            DialogState = request.DialogState,
+        };
+
+        if (request.Intent is { } intent)
+        {
+            result.Intent = new() { Name = intent.Name };
+
+            if (intent.Slots is { } slots)
+            {
+                result.Intent.Slots = slots.Where((p) => p.Value is not null).ToDictionary((p) => p.Key, (p) => new Alexa.NET.Request.Slot()
+                {
+                    Name = p.Value.Name,
+                    Value = p.Value.Value,
+                });
+        }
+
+        return result;
+    }
+
+    private static SessionEndedRequest MapSessionEnd(Models.SessionEndedRequest session)
+    {
+        var result = new SessionEndedRequest()
+        {
+            Reason = Enum.Parse<Reason>(session.Reason.ToString()),
+        };
+
+        if (session.Error is { } error)
+        {
+            result.Error = new()
+            {
+                Message = error.Message,
+                Type = Enum.Parse<ErrorType>(error.Type.ToString()),
+            };
+        }
 
         return result;
     }
