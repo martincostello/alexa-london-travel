cff1b18fcd25ab89451285364f0def7b3b7fcc5f
martincostello <martin@martincostello.com>
2023-12-02 16:11:38 +0000

    Revert "Break skill"
    This reverts commit 0e1b3a4b6b119b25db5f43cbe5fff996f241ee51.

--- src/LondonTravel.Skill/Models/Card.cs
+++ src/LondonTravel.Skill/Models/Card.cs
@@ -1,8 +1,12 @@
 // Copyright (c) Martin Costello, 2017. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+using System.Text.Json.Serialization;
+
 namespace MartinCostello.LondonTravel.Skill.Models;
 
+[JsonDerivedType(typeof(LinkAccountCard))]
+[JsonDerivedType(typeof(StandardCard))]
 public abstract class Card
 {
     public abstract string Type { get; set; }
