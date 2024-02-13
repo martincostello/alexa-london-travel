f77b6440b3bd898d04ebf4681ff27b4fe81a2161
martincostello <martin@martincostello.com>
2024-02-13 19:14:58 +0000

    Update to .NET 9 preview 1 Update to preview 1 of .NET 9.

--- build.ps1
+++ build.ps1
@@ -8,8 +8,6 @@ param(
 $ErrorActionPreference = "Stop"
 $ProgressPreference = "SilentlyContinue"
 
-$env:MSBUILDTERMINALLOGGER = "auto"
-
 $solutionPath = $PSScriptRoot
 $sdkFile = Join-Path $solutionPath "global.json"
 
