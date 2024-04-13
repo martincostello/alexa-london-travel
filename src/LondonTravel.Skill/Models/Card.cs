54261dd25fb43c61f8b4320c9a84b5430616a655
martincostello <martin@martincostello.com>
2024-04-13 10:15:31 +0100

    Support polymorphic deserialization
    Support polymorphic deserialization of the cards using System.Text.Json.

--- src/LondonTravel.Skill/Models/Card.cs
+++ src/LondonTravel.Skill/Models/Card.cs
@@ -5,9 +5,11 @@ using System.Text.Json.Serialization;
 
 namespace MartinCostello.LondonTravel.Skill.Models;
 
-[JsonDerivedType(typeof(LinkAccountCard))]
-[JsonDerivedType(typeof(StandardCard))]
+[JsonDerivedType(typeof(LinkAccountCard), CardTypes.LinkAccount)]
+[JsonDerivedType(typeof(StandardCard), CardTypes.Standard)]
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "type")]
 public abstract class Card
 {
-    public abstract string Type { get; set; }
+    [JsonIgnore]
+    public abstract string Type { get; }
 }
--- src/LondonTravel.Skill/Models/CardTypes.cs
+++ src/LondonTravel.Skill/Models/CardTypes.cs
@@ -1,12 +1,10 @@
 // Copyright (c) Martin Costello, 2017. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using System.Text.Json.Serialization;
-
 namespace MartinCostello.LondonTravel.Skill.Models;
 
-public sealed class LinkAccountCard : Card
+public static class CardTypes
 {
-    [JsonIgnore]
-    public override string Type => CardTypes.LinkAccount;
+    public const string LinkAccount = "LinkAccount";
+    public const string Standard = "Standard";
 }
