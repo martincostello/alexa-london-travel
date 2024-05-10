a4327ca96bae2d90634f6bb4d74e128064b6bb19
martincostello <martin@martincostello.com>
2024-05-10 13:30:02 +0100

    Fix NotSupportedException
    Fix `NotSupportedException` if trying to parse a normal CloudWatch log event as a platform event.

--- test/LondonTravel.Skill.EndToEndTests/CloudWatchLogsFixture.cs
+++ test/LondonTravel.Skill.EndToEndTests/CloudWatchLogsFixture.cs
@@ -189,8 +189,19 @@ public class CloudWatchLogsFixture(IMessageSink diagnosticMessageSink) : IAsyncL
     {
         parsed = default;
 
-        if (JsonSerializer.Deserialize(log.Message, PlatformJsonSerializationContext.Default.PlatformEvent) is not { } @event ||
-            @event is not PlatformReportEvent report ||
+        PlatformEvent? @event;
+
+        try
+        {
+            @event = JsonSerializer.Deserialize(log.Message, PlatformJsonSerializationContext.Default.PlatformEvent);
+        }
+        catch (NotSupportedException)
+        {
+            // Missing the type discriminator, so not a platform event
+            return false;
+        }
+
+        if (@event is not PlatformReportEvent report ||
             report.Record.Metrics is not { } metrics ||
             report.Record.RequestId is not { Length: > 0 } requestId)
         {
