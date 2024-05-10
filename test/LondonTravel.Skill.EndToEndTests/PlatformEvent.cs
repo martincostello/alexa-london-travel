b30c4a973fd52728acf96dc05e1142bea98c56fb
martincostello <martin@martincostello.com>
2024-05-10 14:10:57 +0100

    Add platform.extension
    Go back to distinct events as duplicate polymorphic types aren't supported and add `platform.extension`.

--- test/LondonTravel.Skill.EndToEndTests/PlatformEvent.cs
+++ test/LondonTravel.Skill.EndToEndTests/PlatformEvent.cs
@@ -8,17 +8,11 @@ namespace MartinCostello.LondonTravel.Skill.EndToEndTests;
 /// <summary>
 /// See https://docs.aws.amazon.com/lambda/latest/dg/telemetry-schema-reference.html#telemetry-api-events.
 /// </summary>
+[JsonDerivedType(typeof(PlatformExtensionEvent), PlatformEventType.Extension)]
 [JsonDerivedType(typeof(PlatformInitEvent), PlatformEventType.Initialize)]
 [JsonDerivedType(typeof(PlatformReportEvent), PlatformEventType.Report)]
 [JsonDerivedType(typeof(PlatformStartEvent), PlatformEventType.Start)]
-[JsonDerivedType(typeof(PlatformEmptyEvent), "platform.logsDropped")]
-[JsonDerivedType(typeof(PlatformEmptyEvent), "platform.initReport")]
-[JsonDerivedType(typeof(PlatformEmptyEvent), "platform.initRuntimeDone")]
-[JsonDerivedType(typeof(PlatformEmptyEvent), "platform.restoreReport")]
-[JsonDerivedType(typeof(PlatformEmptyEvent), "platform.restoreRuntimeDone")]
-[JsonDerivedType(typeof(PlatformEmptyEvent), "platform.restoreStart")]
-[JsonDerivedType(typeof(PlatformEmptyEvent), "platform.runtimeDone")]
-[JsonDerivedType(typeof(PlatformEmptyEvent), "platform.telemetrySubscription")]
+[JsonDerivedType(typeof(PlatformTelemetrySubscriptionEvent), PlatformEventType.TelemetrySubscription)]
 [JsonPolymorphic(TypeDiscriminatorPropertyName = "type")]
 internal abstract class PlatformEvent
 {
