f300b56644d54a984f28d1c795a1e369244b0ce1
martincostello <martin@martincostello.com>
2024-05-10 13:06:23 +0100

    Use JSON polymorphism
    Use polymorphism to handle CloudWatch logs.

--- test/LondonTravel.Skill.EndToEndTests/PlatformEvent.cs
+++ test/LondonTravel.Skill.EndToEndTests/PlatformEvent.cs
@@ -8,16 +8,15 @@ namespace LondonTravel.Skill.EndToEndTests;
 /// <summary>
 /// See https://docs.aws.amazon.com/lambda/latest/dg/telemetry-schema-reference.html#telemetry-api-events.
 /// </summary>
-//// TODO Make polymorphic for .NET 9 with AllowOutOfOrderMetadataProperties = true
-////[JsonPolymorphic(TypeDiscriminatorPropertyName = "type")]
-internal sealed class PlatformEvent
+[JsonDerivedType(typeof(PlatformInitEvent), PlatformEventType.Initialize)]
+[JsonDerivedType(typeof(PlatformReportEvent), PlatformEventType.Report)]
+[JsonDerivedType(typeof(PlatformStartEvent), PlatformEventType.Start)]
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "type")]
+internal abstract class PlatformEvent
 {
     [JsonPropertyName("time")]
     public DateTime Timestamp { get; set; }
 
-    [JsonPropertyName("type")]
-    public string Type { get; set; } = default!;
-
-    [JsonPropertyName("record")]
-    public PlatformLogRecord? Record { get; set; }
+    [JsonIgnore]
+    public abstract string Type { get; }
 }
