6880d4c3eff10417b52cf66a13d69d01d97ad208
Martin Costello <martin@martincostello.com>
2024-08-14 21:56:23 +0100

    Use [JsonStringEnumMemberName] (#1386)
    Use `[JsonStringEnumMemberName]` and remove custom converter.

--- src/LondonTravel.Skill/Models/Reason.cs
+++ src/LondonTravel.Skill/Models/Reason.cs
@@ -1,18 +1,18 @@
 // Copyright (c) Martin Costello, 2017. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 
 namespace MartinCostello.LondonTravel.Skill.Models;
 
 public enum Reason
 {
-    [EnumMember(Value = "USER_INITIATED")]
+    [JsonStringEnumMemberName("USER_INITIATED")]
     UserInitiated = 0,
 
-    [EnumMember(Value = "ERROR")]
+    [JsonStringEnumMemberName("ERROR")]
     Error,
 
-    [EnumMember(Value = "EXCEEDED_MAX_REPROMPTS")]
+    [JsonStringEnumMemberName("EXCEEDED_MAX_REPROMPTS")]
     ExceededMaxReprompts,
 }
