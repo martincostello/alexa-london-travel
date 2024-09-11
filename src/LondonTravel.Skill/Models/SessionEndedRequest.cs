67b1f8c53c7706aa4dfcef3597845e11c141c7d3
martincostello <martin@martincostello.com>
2024-09-11 22:49:41 +0100

    Use JSON polymorphism
    Use JSON polymorphism to (de)serialize Alexa requests and responses.

---
+++ src/LondonTravel.Skill/Models/SessionEndedRequest.cs
@@ -0,0 +1,20 @@
+// Copyright (c) Martin Costello, 2017. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Text.Json.Serialization;
+
+namespace MartinCostello.LondonTravel.Skill.Models;
+
+public sealed class SessionEndedRequest : Request
+{
+    [JsonIgnore]
+    public override string Type => "SessionEndedRequest";
+
+    [JsonConverter(typeof(JsonStringEnumConverter<Reason>))]
+    [JsonPropertyName("reason")]
+    public Reason Reason { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyName("error")]
+    public AlexaError Error { get; set; } = default!;
+}
