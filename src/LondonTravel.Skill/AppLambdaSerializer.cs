f1ae630e31ef0ccfa7f10135c6dd29eba5dd3355
martincostello <martin@martincostello.com>
2024-09-12 10:34:28 +0100

    Debug JSON payloads
    Attempt to debug JSON payloads.

--- src/LondonTravel.Skill/AppLambdaSerializer.cs
+++ src/LondonTravel.Skill/AppLambdaSerializer.cs
@@ -10,4 +10,41 @@ public sealed class AppLambdaSerializer() : SourceGeneratorLambdaJsonSerializer<
 {
     protected override JsonSerializerOptions CreateDefaultJsonSerializationOptions()
         => new(AppJsonSerializerContext.Default.Options);
+
+    protected override T InternalDeserialize<T>(byte[] utf8Json)
+    {
+        try
+        {
+            Console.WriteLine(Convert.ToBase64String(utf8Json));
+            return base.InternalDeserialize<T>(utf8Json);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            throw;
+        }
+    }
+
+    protected override void InternalSerialize<T>(Utf8JsonWriter writer, T response)
+    {
+        try
+        {
+            using (var stream = new MemoryStream())
+            {
+                using var writer2 = new Utf8JsonWriter(stream, WriterOptions);
+                base.InternalSerialize(writer2, response);
+                stream.Position = 0L;
+
+                Console.WriteLine(Convert.ToBase64String(stream.ToArray()));
+            }
+
+            Console.WriteLine(response.GetType().ToString());
+            base.InternalSerialize(writer, response);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            throw;
+        }
+    }
 }
