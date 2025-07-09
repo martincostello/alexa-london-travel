f906da18e8a1783e50a66fd55ed7095b4081c645
Martin Costello <martin@martincostello.com>
2025-07-09 12:04:51 +0100

    Observability improvements
    - Set `service.namespace`.
    - Remove `peer.service` with custom names.

--- src/LondonTravel.Skill/Extensions/TelemetryExtensions.cs
+++ src/LondonTravel.Skill/Extensions/TelemetryExtensions.cs
@@ -1,11 +1,7 @@
 // Copyright (c) Martin Costello, 2017. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using System.Collections.Concurrent;
 using System.Diagnostics;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 using OpenTelemetry.Instrumentation.AWSLambda;
 using OpenTelemetry.Instrumentation.Http;
 using OpenTelemetry.Trace;
@@ -14,8 +10,6 @@ namespace MartinCostello.LondonTravel.Skill.Extensions;
 
 internal static class TelemetryExtensions
 {
-    private static readonly ConcurrentDictionary<string, string> ServiceMap = new(StringComparer.OrdinalIgnoreCase);
-
     public static IServiceCollection AddTelemetry(this IServiceCollection services)
     {
         services.AddSingleton((_) => SkillTelemetry.ActivitySource);
@@ -24,7 +18,7 @@ internal static class TelemetryExtensions
                 {
                     builder.SetResourceBuilder(SkillTelemetry.ResourceBuilder)
                            .AddSource(SkillTelemetry.ServiceName)
-                           .AddHttpClientInstrumentation();
+                           .AddHttpClientInstrumentation((p) => p.RecordException = true);
 
                     if (AlexaFunction.IsRunningInAwsLambda())
                     {
@@ -33,51 +27,6 @@ internal static class TelemetryExtensions
                     }
                 });
 
-        services.AddOptions<HttpClientTraceInstrumentationOptions>()
-                .Configure<IServiceProvider>((options, provider) =>
-                {
-                    AddServiceMappings(ServiceMap, provider);
-
-                    options.EnrichWithHttpRequestMessage = EnrichHttpActivity;
-                    options.RecordException = true;
-                });
-
         return services;
-    }
-
-    private static void EnrichHttpActivity(Activity activity, HttpRequestMessage request)
-    {
-        TryEnrichWithPeerService(activity);
-
-        static void TryEnrichWithPeerService(Activity activity)
-        {
-            if (GetTag("server.address", activity.Tags) is { Length: > 0 } hostName)
-            {
-                if (!ServiceMap.TryGetValue(hostName, out string? service))
-                {
-                    service = hostName;
-                }
-
-                activity.AddTag("peer.service", service);
-            }
-        }
-
-        static string? GetTag(string name, IEnumerable<KeyValuePair<string, string?>> tags)
-            => tags.FirstOrDefault((p) => p.Key == name).Value;
-    }
-
-    private static void AddServiceMappings(ConcurrentDictionary<string, string> mappings, IServiceProvider serviceProvider)
-    {
-        var configuration = serviceProvider.GetRequiredService<IConfiguration>();
-        var options = serviceProvider.GetRequiredService<IOptions<SkillConfiguration>>().Value;
-
-        mappings[options.SkillApiUrl.Host] = "Skill API";
-        mappings[options.TflApiUrl.Host] = "TfL API";
-
-        if (configuration["AWS_LAMBDA_RUNTIME_API"] is { Length: > 0 } url &&
-            Uri.TryCreate($"http://{url}", UriKind.Absolute, out var uri))
-        {
-            mappings[uri.Host] = "AWS Lambda Runtime API";
-        }
     }
 }
