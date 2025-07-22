21844aca9e682836b89d3b682ce49e1c164a4300
martincostello <martin@martincostello.com>
2025-07-22 18:15:22 +0100

    Remove unused using
    Leftovers from debugging.

--- src/LondonTravel.Skill/Extensions/TelemetryExtensions.cs
+++ src/LondonTravel.Skill/Extensions/TelemetryExtensions.cs
@@ -3,7 +3,6 @@
 
 using System.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using OpenTelemetry;
 using OpenTelemetry.Instrumentation.AWSLambda;
 using OpenTelemetry.Instrumentation.Http;
