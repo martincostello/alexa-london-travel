e31172829036dd5c9007f63c702933cace7ae9c8
martincostello <martin@martincostello.com>
2025-07-25 16:46:02 +0100

    Add [JsonIgnore]
    Add `[JsonIgnore]` to `Type` overrides as .NET 10 throws an exception rather than inheriting the base property's ignore.

--- test/LondonTravel.Skill.EndToEndTests/PlatformTelemetrySubscriptionEvent.cs
+++ test/LondonTravel.Skill.EndToEndTests/PlatformTelemetrySubscriptionEvent.cs
@@ -8,5 +8,6 @@ namespace MartinCostello.LondonTravel.Skill.EndToEndTests;
 /// </summary>
 internal sealed class PlatformTelemetrySubscriptionEvent : PlatformEvent
 {
+    [System.Text.Json.Serialization.JsonIgnore]
     public override string Type => PlatformEventType.TelemetrySubscription;
 }
