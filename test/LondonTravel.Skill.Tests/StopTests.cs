f872a32cad66a0652a1553ed720d8b371d5166f7
martincostello <martin@martincostello.com>
2025-08-13 16:00:18 +0100

    Remove using statements
    Remove redundant using statements.

--- test/LondonTravel.Skill.Tests/StopTests.cs
+++ test/LondonTravel.Skill.Tests/StopTests.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 using Amazon.Lambda.TestUtilities;
-using MartinCostello.LondonTravel.Skill.Models;
 
 namespace MartinCostello.LondonTravel.Skill;
 
