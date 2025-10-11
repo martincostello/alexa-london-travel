martincostello/alexa-london-travel

commit 4015a7ada9eef0ec82055a8ba4dd75e94456bd6d

Restore metrics (#1870)
- Restore metrics usage with explicit flush and custom exporter settings to see if it makes it work (at the expense of the flush time).
- Add custom metric for skill invocations.
- Fix typo in comment.

Directory.Packages.props
@@ -43,6 +43,7 @@
     <PackageVersion Include="OpenTelemetry.Instrumentation.AWS" Version="1.12.0" />
     <PackageVersion Include="OpenTelemetry.Instrumentation.AWSLambda" Version="1.12.0" />
     <PackageVersion Include="OpenTelemetry.Instrumentation.Http" Version="1.12.0" />
+    <PackageVersion Include="OpenTelemetry.Instrumentation.Process" Version="1.12.0-beta.1" />
     <PackageVersion Include="OpenTelemetry.Resources.Host" Version="1.12.0-beta.1" />
     <PackageVersion Include="OpenTelemetry.Resources.OperatingSystem" Version="1.12.0-beta.1" />
     <PackageVersion Include="OpenTelemetry.Resources.ProcessRuntime" Version="1.12.0-beta.1" />

src/LondonTravel.Skill/Extensions/TelemetryExtensions.cs
@@ -4,8 +4,10 @@
 using System.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
 using OpenTelemetry;
+using OpenTelemetry.Exporter;
 using OpenTelemetry.Instrumentation.AWSLambda;
 using OpenTelemetry.Instrumentation.Http;
+using OpenTelemetry.Metrics;
 using OpenTelemetry.Trace;
 
 namespace MartinCostello.LondonTravel.Skill.Extensions;
@@ -16,6 +18,8 @@ internal static class TelemetryExtensions
 
     public static IServiceCollection AddTelemetry(this IServiceCollection services)
     {
+        services.AddMetrics();
+        services.AddSingleton<SkillMetrics>();
         services.AddSingleton((_) => SkillTelemetry.ActivitySource);
 
         bool isRunningInLambda = AlexaFunction.IsRunningInAwsLambda();
@@ -25,9 +29,24 @@ internal static class TelemetryExtensions
         if (isRunningInLambda)
         {
             builder.UseOtlpExporter();
+            services.Configure<OtlpExporterOptions>((options) =>
+            {
+                options.BatchExportProcessorOptions.ExporterTimeoutMilliseconds = 3_000;
+                options.BatchExportProcessorOptions.MaxExportBatchSize = 10;
+                options.BatchExportProcessorOptions.MaxQueueSize = 100;
+                options.BatchExportProcessorOptions.ScheduledDelayMilliseconds = 1_000;
+            });
         }
 
-        builder.WithTracing((builder) =>
+        builder.WithMetrics((builder) =>
+               {
+                   builder.SetResourceBuilder(SkillTelemetry.ResourceBuilder)
+                          .AddHttpClientInstrumentation()
+                          .AddProcessInstrumentation()
+                          .AddMeter(SkillTelemetry.ServiceName)
+                          .AddMeter("System.Runtime");
+               })
+               .WithTracing((builder) =>
                {
                    builder.SetResourceBuilder(SkillTelemetry.ResourceBuilder)
                           .AddSource(SkillTelemetry.ServiceName)

src/LondonTravel.Skill/LondonTravel.Skill.csproj
@@ -41,6 +41,7 @@
     <PackageReference Include="OpenTelemetry.Instrumentation.AWS" />
     <PackageReference Include="OpenTelemetry.Instrumentation.AWSLambda" />
     <PackageReference Include="OpenTelemetry.Instrumentation.Http" />
+    <PackageReference Include="OpenTelemetry.Instrumentation.Process" />
     <PackageReference Include="OpenTelemetry.Resources.Host" />
     <PackageReference Include="OpenTelemetry.Resources.OperatingSystem" />
     <PackageReference Include="OpenTelemetry.Resources.ProcessRuntime" />

src/LondonTravel.Skill/AlexaFunction.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using OpenTelemetry.Metrics;
 
 namespace MartinCostello.LondonTravel.Skill;
 
@@ -56,18 +57,25 @@ public class AlexaFunction : IAsyncDisposable, IDisposable
     /// Handles a request to the skill as an asynchronous operation.
     /// </summary>
     /// <param name="request">The skill request.</param>
-    /// <param name="context">The Lamda request context.</param>
+    /// <param name="context">The Lambda request context.</param>
     /// <returns>
     /// A <see cref="Task{TResult}"/> representing the asynchronous operation to get the skill's response.
     /// </returns>
     public async Task<SkillResponse> HandlerAsync(SkillRequest request, ILambdaContext context)
     {
         EnsureInitialized();
-        return await OpenTelemetry.Instrumentation.AWSLambda.AWSLambdaWrapper.TraceAsync(
+
+        var meterProvider = _serviceProvider.GetRequiredService<MeterProvider>();
+
+        var response = await OpenTelemetry.Instrumentation.AWSLambda.AWSLambdaWrapper.TraceAsync(
             _serviceProvider.GetRequiredService<OpenTelemetry.Trace.TracerProvider>(),
             HandlerCoreAsync,
             request,
             context);
+
+        meterProvider.ForceFlush();
+
+        return response;
     }
 
     /// <summary>
@@ -167,6 +175,9 @@ public class AlexaFunction : IAsyncDisposable, IDisposable
     {
         var handler = _serviceProvider!.GetRequiredService<FunctionHandler>();
         var logger = _serviceProvider!.GetRequiredService<ILogger<AlexaFunction>>();
+
+        var metrics = _serviceProvider!.GetRequiredService<SkillMetrics>();
+        metrics.SkillInvoked(request.Request.Type);
 
         using var activity = SkillTelemetry.ActivitySource.StartActivity("Skill Request");
 

src/LondonTravel.Skill/SkillMetrics.cs
@@ -1,0 +1,27 @@
+// Copyright (c) Martin Costello, 2017. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Diagnostics.Metrics;
+
+namespace MartinCostello.LondonTravel.Skill;
+
+internal sealed class SkillMetrics
+{
+    private readonly Meter _meter;
+    private readonly Counter<long> _skillInvocationCounter;
+
+    public SkillMetrics(IMeterFactory meterFactory)
+    {
+        _meter = meterFactory.Create(SkillTelemetry.ServiceName, SkillTelemetry.ServiceVersion);
+
+        _skillInvocationCounter = _meter.CreateCounter<long>(
+            "londontravel.skill.invocations",
+            unit: "{count}",
+            description: "The number of Alexa Skill invocations.");
+    }
+
+    public void Dispose() => _meter?.Dispose();
+
+    public void SkillInvoked(string? requestType)
+        => _skillInvocationCounter.Add(1, new KeyValuePair<string, object?>("alexa.request.type", requestType));
+}

commit d03e1c65bd1b041d02b2eca1b326517bf3ba6b61

Remove OpenTelemetry.Instrumentation.Process
Remove OpenTelemetry.Instrumentation.Process, as there's a possibility it is the cause of the performance regression originally identified from #1870 that there was an attempted fix for in #1873 that didn't actually work.

Directory.Packages.props
@@ -43,7 +43,6 @@
     <PackageVersion Include="OpenTelemetry.Instrumentation.AWS" Version="1.12.1" />
     <PackageVersion Include="OpenTelemetry.Instrumentation.AWSLambda" Version="1.12.1" />
     <PackageVersion Include="OpenTelemetry.Instrumentation.Http" Version="1.12.0" />
-    <PackageVersion Include="OpenTelemetry.Instrumentation.Process" Version="1.12.0-beta.1" />
     <PackageVersion Include="OpenTelemetry.Resources.Host" Version="1.12.0-beta.1" />
     <PackageVersion Include="OpenTelemetry.Resources.OperatingSystem" Version="1.12.0-beta.1" />
     <PackageVersion Include="OpenTelemetry.Resources.ProcessRuntime" Version="1.12.0-beta.1" />

src/LondonTravel.Skill/Extensions/TelemetryExtensions.cs
@@ -39,7 +39,6 @@ internal static class TelemetryExtensions
                 builder.SetResourceBuilder(SkillTelemetry.ResourceBuilder)
                        .AddAWSInstrumentation()
                        .AddHttpClientInstrumentation()
-                       .AddProcessInstrumentation()
                        .AddMeter(SkillTelemetry.ServiceName)
                        .AddMeter("System.Runtime");
             });

src/LondonTravel.Skill/LondonTravel.Skill.csproj
@@ -41,7 +41,6 @@
     <PackageReference Include="OpenTelemetry.Instrumentation.AWS" />
     <PackageReference Include="OpenTelemetry.Instrumentation.AWSLambda" />
     <PackageReference Include="OpenTelemetry.Instrumentation.Http" />
-    <PackageReference Include="OpenTelemetry.Instrumentation.Process" />
     <PackageReference Include="OpenTelemetry.Resources.Host" />
     <PackageReference Include="OpenTelemetry.Resources.OperatingSystem" />
     <PackageReference Include="OpenTelemetry.Resources.ProcessRuntime" />