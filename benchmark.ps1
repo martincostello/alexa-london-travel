1290d6dfeefef7d266eaeb2da7b4111fd99c1fd6
Martin Costello <martin@martincostello.com>
2026-06-23 12:05:04 +0100

    Update BenchmarkDotNet
    Update package and enable new option for consistent behaviour with 0.15.8 to compare with .NET 10.

--- benchmark.ps1
+++ benchmark.ps1
@@ -62,7 +62,9 @@ $benchmarks = (Join-Path $solutionPath "perf" "LondonTravel.Skill.Benchmarks" "L
 
 Write-Information "Running benchmarks..."
 
-$additionalArgs = @()
+$additionalArgs = @(
+    "--consumeTasksSynchronously" # For backwards compatibility with BenchmarkDotNet versions before 0.16.0
+)
 
 if (-Not [string]::IsNullOrEmpty($Filter)) {
     $additionalArgs += "--filter"
